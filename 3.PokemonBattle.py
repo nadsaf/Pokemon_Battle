dfa2ebf9a6df03df6177f6ed4a82723662364cc8
nadsaf <saffira.nadya@gmail.com>
2019-07-15 22:48:38 +0700

    2

--- 3.PokemonBattle.py
+++ 3.PokemonBattle.py
@@ -32,45 +32,48 @@ def result():
             nama_pokemon2 = data2['forms'][0]['name']           
             url_image1 = data1['sprites']['front_default']
             url_image2 = data2['sprites']['front_default']
+             
             data1 = pokemon[pokemon['Name'] == nama_pokemon1.capitalize()][['HP','Attack', 'Defense', 'Sp. Atk', 'Sp. Def', 'Speed']].values.tolist()
             data2 = pokemon[pokemon['Name'] == nama_pokemon2.capitalize()][['HP','Attack', 'Defense', 'Sp. Atk', 'Sp. Def', 'Speed']].values.tolist()
-            
-            
-            img = io.BytesIO()
-            Label = ['HP', 'Attack', 'Defense', 'Sp Attack', 'Sp Defense', 'Speed', ]
-            
-            x = np.arange(len(Label))
-            width = 0.3
-            plt.bar(x - width/2, data1[0], width, label=nama_pokemon1.capitalize(), color='pink')
-            plt.bar(x + width/2, data2[0], width, label=nama_pokemon2.capitalize(), color='#00688b')
-            plt.xticks(ticks=x, labels=Label)
-            plt.legend()
-            plt.tight_layout()
-            plt.savefig(img, format='png', transparent=True)
-            plt.close()
-            img.seek(0)
+            if len(data1) == 0 or len(data2)== 0:
+                return render_template('notFound.html')
+            else:                      
+                img = io.BytesIO()
+                Label = ['HP', 'Attack', 'Defense', 'Sp Attack', 'Sp Defense', 'Speed', ]
+                
+                x = np.arange(len(Label))
+                width = 0.3
+                plt.bar(x - width/2, data1[0], width, label=nama_pokemon1.capitalize(), color='pink')
+                plt.bar(x + width/2, data2[0], width, label=nama_pokemon2.capitalize(), color='#00688b')
+                plt.xticks(ticks=x, labels=Label)
+                plt.legend()
+                plt.tight_layout()
+                plt.savefig(img, format='png', transparent=True)
+                plt.close()
+                img.seek(0)
 
-            plot_url = base64.b64encode(img.getvalue()).decode()
-            input_data = data1[0]
-            input_data.extend(data2[0])
-            prediksi = model.predict([input_data])[0]
-            if prediksi == 0:
-                winner = nama_pokemon2.upper()
-                probabilitas = round(model.predict_proba([input_data])[0][0]*100, 2)
-            else:
-                winner = nama_pokemon1.upper()
-                probabilitas = round(model.predict_proba([input_data])[0][1]*100, 2)
-            print(probabilitas)
-            hasil = {
-            'nama_pokemon1' : nama_pokemon1.upper(), 'nama_pokemon2' : nama_pokemon2.upper(),
-            'url_image1' : url_image1, 'url_image2' : url_image2,
-            'winner' : winner, 'probabilitas' : probabilitas
-            }
-            print(data1)
-            print(data2)
-            print(winner)
-            
-            return render_template('result.html', hasil=hasil, plot_url=plot_url )
+                plot_url = base64.b64encode(img.getvalue()).decode()
+
+                input_data = data1[0]
+                input_data.extend(data2[0])
+                prediksi = model.predict([input_data])[0]
+                print(prediksi)
+                if prediksi == 0:
+                    winner = nama_pokemon2.upper()
+                    probabilitas = round(model.predict_proba([input_data])[0][0]*100, 2)
+                else:
+                    winner = nama_pokemon1.upper()
+                    probabilitas = round(model.predict_proba([input_data])[0][1]*100, 2)
+                print(probabilitas)
+                hasil = {
+                'nama_pokemon1' : nama_pokemon1.upper(), 'nama_pokemon2' : nama_pokemon2.upper(),
+                'url_image1' : url_image1, 'url_image2' : url_image2,
+                'winner' : winner, 'probabilitas' : probabilitas
+                }
+                print(winner)
+                
+                return render_template('result.html', hasil=hasil, plot_url=plot_url )
+
     else:
         return redirect(url_for('home'))
 #========================================================================================================
