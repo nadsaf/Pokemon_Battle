3ea2aa721886af0e38f7c0cb843e75bdca01fbdd
nadsaf <saffira.nadya@gmail.com>
2019-07-15 21:47:56 +0700

    first

---
+++ 3.PokemonBattle.py
@@ -0,0 +1,88 @@
+from flask import Flask, abort, render_template, request, send_from_directory, url_for, redirect,jsonify
+import requests
+import json
+import joblib
+import matplotlib.pyplot as plt
+import numpy as np
+import io
+import base64
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    return render_template('home.html')
+
+@app.route('/result', methods = ['POST', 'GET'])
+def result():
+    if request.method == 'POST':
+        poke1= request.form['pokemon1']
+        poke2= request.form['pokemon2']
+        poki1 = poke1.lower()
+        poki2 = poke2.lower()
+        url_poke1 = 'https://pokeapi.co/api/v2/pokemon/'+poki1
+        url_poke2 = 'https://pokeapi.co/api/v2/pokemon/'+poki2
+        raw_data1 = requests.get(url_poke1)
+        raw_data2 = requests.get(url_poke2)
+        if str(raw_data1) == '<Response [404]>' or str(raw_data2) == '<Response [404]>':
+            return render_template('notFound.html')
+        else:    
+            data1 = raw_data1.json()
+            data2 = raw_data2.json()
+            nama_pokemon1 = data1['forms'][0]['name']
+            nama_pokemon2 = data2['forms'][0]['name']           
+            url_image1 = data1['sprites']['front_default']
+            url_image2 = data2['sprites']['front_default']
+            data1 = pokemon[pokemon['Name'] == nama_pokemon1.capitalize()][['HP','Attack', 'Defense', 'Sp. Atk', 'Sp. Def', 'Speed']].values.tolist()
+            data2 = pokemon[pokemon['Name'] == nama_pokemon2.capitalize()][['HP','Attack', 'Defense', 'Sp. Atk', 'Sp. Def', 'Speed']].values.tolist()
+            
+            
+            img = io.BytesIO()
+            Label = ['HP', 'Attack', 'Defense', 'Sp Attack', 'Sp Defense', 'Speed', ]
+            
+            x = np.arange(len(Label))
+            width = 0.3
+            plt.bar(x - width/2, data1[0], width, label=nama_pokemon1.capitalize(), color='pink')
+            plt.bar(x + width/2, data2[0], width, label=nama_pokemon2.capitalize(), color='#00688b')
+            plt.xticks(ticks=x, labels=Label)
+            plt.legend()
+            plt.tight_layout()
+            plt.savefig(img, format='png', transparent=True)
+            plt.close()
+            img.seek(0)
+
+            plot_url = base64.b64encode(img.getvalue()).decode()
+            input_data = data1[0]
+            input_data.extend(data2[0])
+            prediksi = model.predict([input_data])[0]
+            if prediksi == 0:
+                winner = nama_pokemon2.upper()
+                probabilitas = round(model.predict_proba([input_data])[0][0]*100, 2)
+            else:
+                winner = nama_pokemon1.upper()
+                probabilitas = round(model.predict_proba([input_data])[0][1]*100, 2)
+            print(probabilitas)
+            hasil = {
+            'nama_pokemon1' : nama_pokemon1.upper(), 'nama_pokemon2' : nama_pokemon2.upper(),
+            'url_image1' : url_image1, 'url_image2' : url_image2,
+            'winner' : winner, 'probabilitas' : probabilitas
+            }
+            print(data1)
+            print(data2)
+            print(winner)
+            
+            return render_template('result.html', hasil=hasil, plot_url=plot_url )
+    else:
+        return redirect(url_for('home'))
+#========================================================================================================
+# TEMP IMAGE
+@app.route('/filetemp/<path:path>')                           
+def filetemp(path):
+    return send_from_directory('./templates/image', path)
+
+@app.errorhandler(404)                                              
+def notFound(error) :                                               
+    return render_template('error.html')
+
+if __name__ == '__main__':
+    pokemon, model = joblib.load('modelpokemon')                 
+    app.run(debug = True)  
---
+++ 3.py
@@ -0,0 +1,70 @@
+import pandas as pd
+import numpy as np
+
+pokemon = pd.read_csv('pokemon.csv')
+pokemon = pokemon.drop(['Type 1', 'Type 2', 'Generation', 'Legendary'], axis=1)
+pokemon = pokemon.rename(columns={'#' : 'id'})
+
+combat = pd.read_csv('combats.csv')
+print(combat)
+
+merged1 = combat.merge(pokemon, how = "left", left_on = "First_pokemon", right_on = "id")
+merged2 = combat.merge(pokemon, how = "left", left_on = "Second_pokemon", right_on = "id")
+
+merged2.drop(["Winner","First_pokemon","Second_pokemon"], axis = 1, inplace = True)
+
+for i in merged2.columns : 
+    merged2.rename(columns = {i : i + "_2"}, inplace = True)
+
+df_combat = pd.concat([merged1,merged2], axis = 1)
+# print(df_combat)
+df_combat['is_firstwin'] = 0
+df_combat.loc[df_combat['Winner'] == df_combat['First_pokemon'], 'is_firstwin'] = 1
+# print(df_combat)
+
+#========================================================================================
+X = df_combat.drop(['First_pokemon', 'Second_pokemon', 'Winner', 'id', 'id_2', 'Name', 'Name_2', 'is_firstwin'], axis=1)
+Y = df_combat['is_firstwin']
+
+#========================================================================================
+from sklearn.ensemble import GradientBoostingClassifier
+model = GradientBoostingClassifier()
+model.fit(X, Y)
+print(model.score(X,Y))
+print(model.predict_proba(X.iloc[0].values.reshape(1,-1)))
+
+#===========================================================
+pokemon1 = 'pikachu'
+pokemon2 = 'bulbasaur'
+
+data1 = pokemon[pokemon['Name'] == pokemon1.capitalize()][['HP','Attack', 'Defense', 'Sp. Atk', 'Sp. Def', 'Speed']].values.tolist()
+data2 = pokemon[pokemon['Name'] == pokemon2.capitalize()][['HP','Attack', 'Defense', 'Sp. Atk', 'Sp. Def', 'Speed']].values.tolist()
+
+import matplotlib.pyplot as plt
+import seaborn as sns
+Label = ['HP', 'Attack', 'Defense', 'Sp Attack', 'Sp Defense', 'Speed', ]
+
+x = np.arange(len(Label))
+width = 0.3
+fig, ax = plt.subplots()
+bar1 = ax.bar(x - width/2, data1[0], width, label='aaa', color='pink')
+bar2 = ax.bar(x + width/2, data2[0], width, label='bbbb', color='#00688b')
+plt.xticks(ticks=x, labels=Label)
+ax.set_xticks(x)
+ax.set_xticklabels(Label)
+ax.legend()
+plt.tight_layout()
+plt.show()
+
+input_data = data1[0]
+input_data.extend(data2[0])
+print(data1[0])
+print(data2[0])
+
+print(input_data)
+prediksi = model.predict([input_data])[0]
+print(model.predict_proba([input_data])[0][0]*100)
+print(prediksi)
+# import joblib
+# joblib.dump([pokemon, model], 'modelpokemon')
+
